bcb8912c0a5ed07b6380daeaddaba1f1d5597002
reismgadsden <rgadsden287@gmail.com>
2022-01-19 14:13:39 -0500

    Added method headers

--- BurteForceCoinSolution.py
+++ BurteForceCoinSolution.py
@@ -5,3 +5,18 @@ author: Reis Gadsden
 version: 19/01/2022
 class: CS-5531 @ Appalachian State University
 """
+
+def main():
+    pass
+
+
+def bruteForceChange(val, currency):
+    pass
+
+
+def convertToChange(val, currency):
+    pass
+
+
+if __name__ == "__main__":
+    main()
