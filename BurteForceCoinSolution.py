8036234d059b0b7b4c554c7d9e6c5eb71440e244
reismgadsden <rgadsden287@gmail.com>
2022-01-21 16:51:20 -0500

    Implemented all methods succesfully

--- BurteForceCoinSolution.py
+++ BurteForceCoinSolution.py
@@ -8,7 +8,6 @@ class: CS-5531 @ Appalachian State University
 import re
 import numpy as np
 
-STATIC_MAX_VAL = 0
 NUM_PATTERN = re.compile("^[0-9]*$")
 
 def main():
@@ -45,42 +44,68 @@ def main():
             break
         else:
             print("Invalid input! Try again.")
-
-    bruteForceChange(val, currency)
+    print(bruteForceChange(val, currency))
 
 
 
 def bruteForceChange(val, currency):
-    pass
+    max_val = getMaxVal(currency)
+    smallest_number = np.inf
+    best_change = None
+
+    for i in range(0, max_val + 1):
+        coins = convertToChange(i, currency)
+        coin_value = getTotalValue(currency, coins)
+        if coin_value == val:
+            num_coins = 0
+            for x in coins:
+                num_coins += x
+            if num_coins < smallest_number:
+                smallest_number = num_coins
+                best_change = coins
+
+    return best_change
+
+
 
 
 def convertToChange(val, currency):
     # adds values up to max value
-    counter = 1
+    counter = len(currency) - 1
     max_val = val
-    max_converted = np.zeros(1, len(currency))
+    max_converted = []
     while True:
-        mod_val = max_val % (currency[-counter] + 1)
-        max_val = max_val // (currency[-counter] + 1)
-        max_converted[0][len(currency) - counter] = mod_val
+        mod_val = max_val % (currency[counter] + 1)
+        max_val = max_val // (currency[counter] + 1)
+        if len(max_converted) != len(currency):
+            max_converted.insert(0, mod_val)
         if max_val == 0 or max_val == -1:
             break
-        counter += 1
-        if counter > len(currency):
-            counter = 1
+        counter -= 1
+        if counter == 0:
+            counter = len(currency) - 1
+    while len(max_converted) != len(currency):
+        max_converted.insert(0, 0)
 
-    return tuple(map(tuple, max_converted))
+    return tuple(max_converted)
 
 
 def getMaxVal(currency):
     # calculates a max value for the given array
     max_val = 1
-    count_up = []
     for i in currency:
         max_val *= (i + 1)
-        count_up.append(0)
-    max_val -= 1
-    STATIC_MAX_VAL = max_val
+    return max_val - 1
+
+
+def getTotalValue(currency, num_coins):
+    counter = 0
+    sum = 0
+
+    for x in num_coins:
+        sum += currency[counter] * x
+        counter += 1
+    return sum
 
 
 if __name__ == "__main__":
