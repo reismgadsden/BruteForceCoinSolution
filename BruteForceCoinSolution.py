c7f50e96435e223b6e8866dc29e64bc7ca4305c4
reismgadsden <rgadsden287@gmail.com>
2022-01-25 12:26:45 -0500

    Updated header comment

--- BruteForceCoinSolution.py
+++ BruteForceCoinSolution.py
@@ -4,6 +4,7 @@ Brute force algorithm to calculate the most efficient change.
 author: Reis Gadsden
 version: 19/01/2022
 class: CS-5531 @ Appalachian State University
+git: https://github.com/reismgadsden/BruteForceCoinSolution
 """
 import re
 import numpy as np
